ad9d224e865ab7210a348724b1f7b95b12e9a06b
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-27 23:25:40 -0800

    Initial Commit

---
+++ CampusApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CampusApp
+//
+//  Created by Aristotle on 2017-02-27.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
