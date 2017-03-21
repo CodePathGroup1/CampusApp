b30426ae552b289173c975a5b67e6a329ee96e84
bwong <bwong@mwongs-MacBook-Pro.local>
2017-03-20 21:03:28 -0700

    EventViewVC created, DetailEventViewVC created.

---
+++ CampusApp/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  CampusApp
+//
+//  Created by bwong on 3/20/17.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
