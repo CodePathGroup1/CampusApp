bbebd7c2543efa84eada1cdf45de3cba4f72b52d
Thomas Zhu <thomashzhu@Thomass-Air.att.net>
2017-03-22 11:22:33 -0700

    Set up map view (without data yet)

--- CampusApp/EventDetailViewController.swift
+++ CampusApp/EventDetailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class EventDetailViewController: UIViewController {
+class EventDetailViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UITextView!
@@ -25,6 +25,21 @@ class EventDetailViewController: UIViewController {
         descriptionLabel.text = event.description
         
         mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.layer.cornerRadius = 8.0
+        mapView.clipsToBounds = true
+        
+        /*
+        if let latitude = event.latitude, let longitude = event.longitude {
+            let annotation: MKAnnotation = {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                return annotation
+            }()
+            
+            mapView.showAnnotations([annotation], animated: false)
+        }
+        */
         
         print(event)
     }
