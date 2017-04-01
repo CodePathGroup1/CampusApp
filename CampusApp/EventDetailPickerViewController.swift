99b936c2179a59a8737f71f882b46b0e847b7114
thomashzhu <dev@thomaszhu.com>
2017-03-31 21:30:23 -0700

    Introduced picker view to new event view controller

---
+++ CampusApp/EventDetailPickerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EventDetailPickerViewController.swift
+//  CampusApp
+//
+//  Created by Thomas Zhu on 3/31/17.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailPickerViewController: UIViewController {
+
+    @IBOutlet weak var inputTextField: RoundTextField!
+    @IBOutlet weak var inputPickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        
+    }
+}
