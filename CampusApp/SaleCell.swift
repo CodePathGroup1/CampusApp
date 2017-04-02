943aab06f0b5daddfeca54da294bda506c61a1a2
bwong <bwong@mwongs-MacBook-Pro.local>
2017-04-02 16:22:13 -0700

    SaleVC, SaleCell, and SaleDetailVC created

---
+++ CampusApp/SaleCell.swift
@@ -0,0 +1,24 @@
+//
+//  SaleCell.swift
+//  CampusApp
+//
+//  Created by bwong on 4/2/17.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class SaleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CampusApp/SaleDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SaleDetailViewController.swift
+//  CampusApp
+//
+//  Created by bwong on 4/2/17.
+//  Copyright © 2017 HLPostman. All rights reserved.
+//
+
+import UIKit
+
+class SaleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
